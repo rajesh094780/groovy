6db683bb5377cd27b516b6dfb3a5ae9a3e27b0b3
rajesh094780 <37009281+rajesh094780@users.noreply.github.com>
2018-10-23 19:16:26 +0530

    Update first.groovy

--- first.groovy
+++ first.groovy
@@ -1,8 +1,12 @@
 def test1(){
     def targetBranch = (params.upstreamBranch ?: env.BRANCH_NAME).toLowerCase()
     def clusterMeta = [:]
-
     println "On branch $env.BRANCH_NAME ; targetBranch is |$targetBranch|"
+    switch (targetBranch) {
+        case ~/master/:
+            println targetBranch
+            break
+    }
     
 }
 
